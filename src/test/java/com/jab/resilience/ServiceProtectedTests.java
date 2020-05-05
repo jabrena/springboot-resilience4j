9d1136e2339b66c2857d3ff0ef0d0a261fc95404
jabrena <bren@juanantonio.info>
2020-05-05 19:37:58 +0200

    Minor change

--- src/test/java/com/jab/resilience/ServiceProtectedTests.java
+++ src/test/java/com/jab/resilience/ServiceProtectedTests.java
@@ -9,8 +9,6 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
