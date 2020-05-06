fad4d397b2407896a27be8ceee303d758224005d
jabrena <bren@juanantonio.info>
2020-05-06 09:58:11 +0200

    Minor change

--- src/test/java/com/jab/resilience/MainApplicationTests.java
+++ src/test/java/com/jab/resilience/MainApplicationTests.java
@@ -1,14 +1,12 @@
 package com.jab.resilience;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MainApplicationTests {
 
-    @Disabled
-	@Test
+    @Test
 	void contextLoads() {
 
 	}
